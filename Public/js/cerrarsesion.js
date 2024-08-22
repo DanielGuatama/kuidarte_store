c777ad706ea5046810a3c761e8d1aedbb477772c
daniel Guatama <edaniel.guatama@gmail.com>
2024-08-21 20:28:55 -0500

    comentarios cerrarsesion y clientes js

--- Public/js/cerrarsesion.js
+++ Public/js/cerrarsesion.js
@@ -1,9 +1,16 @@
+// Escucha el evento de clic en el botón 'cerrarSesion'
 document.getElementById('cerrarSesion').addEventListener('click', function() {
-    fetch('/logout')
+    // Declarar la variable para la URL de cierre de sesión
+    const logoutUrl = '/logout'; // URL de la API para cerrar sesión
+
+    // Realiza la solicitud de cierre de sesión
+    fetch(logoutUrl)
     .then(() => {
+        // Redirige al usuario a la página de inicio
         window.location.href = '/index.html';
     })
     .catch(error => {
+        // Maneja cualquier error que ocurra durante la solicitud
         console.error('Error:', error);
     });
 });
--- Public/js/clientes.js
+++ Public/js/clientes.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Declaración de variables para los elementos del DOM
     const buscarClienteForm = document.getElementById('buscarClienteForm');
     const clienteEncontradoSection = document.getElementById('clienteEncontradoSection');
     const clienteEncontradoDiv = document.getElementById('clienteEncontrado');
@@ -9,69 +10,73 @@ document.addEventListener('DOMContentLoaded', () => {
     const editarClienteBtn = document.getElementById('editarClienteBtn');
     const eliminarClienteBtn = document.getElementById('eliminarClienteBtn');
     
-    
     // Función para buscar cliente
     buscarClienteForm.addEventListener('submit', event => {
-    event.preventDefault(); // Previene el comportamiento predeterminado
-    
-    const formData = new FormData(buscarClienteForm);
-    const id_cliente = formData.get('id_cliente');
-    
-    if (!id_cliente) {
-        alert('Por favor, ingrese un ID para buscar.');
-        return;
-    }
+        event.preventDefault(); // Previene el comportamiento predeterminado
+        
+        // Obtención de datos del formulario
+        const formData = new FormData(buscarClienteForm);
+        const id_cliente = formData.get('id_cliente');
+        
+        // Validación del ID del cliente
+        if (!id_cliente) {
+            alert('Por favor, ingrese un ID para buscar.');
+            return;
+        }
 
-    const query = `/clientes/${id_cliente}`;
+        const query = `/clientes/${id_cliente}`;
 
-    fetch(query)
-        .then(response => {
-            if (!response.ok) {
-                // Manejo de errores HTTP
-                if (response.status === 404) {
-                    alert('Cliente no encontrado.');
-                } else {
-                    alert('Ocurrió un error al buscar el cliente.');
+        // Llamada a la API para buscar el cliente
+        fetch(query)
+            .then(response => {
+                if (!response.ok) {
+                    // Manejo de errores HTTP
+                    if (response.status === 404) {
+                        alert('Cliente no encontrado.');
+                    } else {
+                        alert('Ocurrió un error al buscar el cliente.');
+                    }
+                    throw new Error('Error al buscar cliente');
                 }
-                throw new Error('Error al buscar cliente');
-            }
-            return response.json();
-        })
-        .then(cliente => {
-            // Verifica si se recibió un cliente
-            if (!cliente) {
-                alert('Cliente no encontrado.');
-                return;
-            }
+                return response.json();
+            })
+            .then(cliente => {
+                // Verifica si se recibió un cliente
+                if (!cliente) {
+                    alert('Cliente no encontrado.');
+                    return;
+                }
 
-            // Muestra la información del cliente encontrado
-            clienteEncontradoDiv.innerHTML = `
-                <p>ID: ${cliente.id_cliente}</p>
-                <p>Nombre: ${cliente.nombre}</p>
-                <p>Cédula/NIT: ${cliente.cedula_nit}</p>
-                <p>Correo: ${cliente.correo}</p>
-                <p>Teléfono: ${cliente.telefono || ''}</p>
-                <p>Dirección: ${cliente.direccion || ''}</p>
-                <p>Ciudad: ${cliente.ciudad || ''}</p>
-            `;
-            clienteEncontradoSection.style.display = 'block';
+                // Muestra la información del cliente encontrado
+                clienteEncontradoDiv.innerHTML = `
+                    <p>ID: ${cliente.id_cliente}</p>
+                    <p>Nombre: ${cliente.nombre}</p>
+                    <p>Cédula/NIT: ${cliente.cedula_nit}</p>
+                    <p>Correo: ${cliente.correo}</p>
+                    <p>Teléfono: ${cliente.telefono || ''}</p>
+                    <p>Dirección: ${cliente.direccion || ''}</p>
+                    <p>Ciudad: ${cliente.ciudad || ''}</p>
+                `;
+                clienteEncontradoSection.style.display = 'block';
 
-            // Asocia los botones de editar y eliminar con las funciones correspondientes
-            editarClienteBtn.onclick = () => editCliente(cliente.id_cliente);
-            eliminarClienteBtn.onclick = () => deleteCliente(cliente.id_cliente);
-        })
-        .catch(error => {
-            console.error('Error al buscar el cliente:', error);
-        })
-        .finally(() => {
-            // Limpia el campo de búsqueda después de realizar la búsqueda
-            buscarClienteForm.reset();
-        });
+                // Asocia los botones de editar y eliminar con las funciones correspondientes
+                editarClienteBtn.onclick = () => editCliente(cliente.id_cliente);
+                eliminarClienteBtn.onclick = () => deleteCliente(cliente.id_cliente);
+            })
+            .catch(error => {
+                console.error('Error al buscar el cliente:', error);
+            })
+            .finally(() => {
+                // Limpia el campo de búsqueda después de realizar la búsqueda
+                buscarClienteForm.reset();
+            });
     });
 
     // Función para agregar cliente
     addClienteForm.addEventListener('submit', event => {
-    event.preventDefault();
+        event.preventDefault(); // Previene el comportamiento predeterminado
+        
+        // Obtención de datos del formulario
         const formData = new FormData(addClienteForm);
         const data = {
             nombre: formData.get('nombre'),
@@ -80,23 +85,24 @@ document.addEventListener('DOMContentLoaded', () => {
             telefono: formData.get('telefono'),
             direccion: formData.get('direccion'),
             ciudad: formData.get('ciudad')
-            };
+        };
     
-            fetch('/clientes', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-            .then(response => response.text())
-            .then(message => {
-                alert(message);
-                addClienteForm.reset();
-            });
+        // Llamada a la API para agregar un nuevo cliente
+        fetch('/clientes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.text())
+        .then(message => {
+            alert(message);
+            addClienteForm.reset(); // Limpia el formulario después de agregar
         });
-    
+    });
 
+    // Función para editar cliente
     function editCliente(id) {
         fetch(`/clientes/${id}`)
             .then(response => response.json())
@@ -116,8 +122,11 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     }
 
+    // Función para manejar la edición del cliente
     editClienteForm.addEventListener('submit', event => {
-        event.preventDefault();
+        event.preventDefault(); // Previene el comportamiento predeterminado
+        
+        // Obtención de datos del formulario de edición
         const formData = new FormData(editClienteForm);
         const data = {
             id_cliente: formData.get('id_cliente'),
@@ -129,6 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
             ciudad: formData.get('ciudad')
         };
 
+        // Llamada a la API para actualizar el cliente
         fetch(`/clientes/${data.id_cliente}`, {
             method: 'PUT',
             headers: {
@@ -143,10 +153,12 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // Función para cancelar la edición
     cancelEditButton.addEventListener('click', () => {
         editSection.style.display = 'none'; // Ocultar formulario de edición
     });
 
+    // Función para eliminar cliente
     function deleteCliente(id) {
         fetch(`/clientes/${id}`, {
             method: 'DELETE'
@@ -154,7 +166,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.text())
         .then(message => {
             alert(message);
-            clienteEncontradoSection.style.display = 'none';
+            clienteEncontradoSection.style.display = 'none'; // Ocultar sección de cliente encontrado
         });
     }
 });
